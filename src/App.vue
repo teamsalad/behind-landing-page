450142da150ff365e43991b20364956333056e78
Youngjun Choi <yjaquians@gmail.com>
2019-08-27 22:46:43 +0900

    initial commit - started email subscription stuff

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>{{ title }}</span>
+        <span class="font-weight-light">{{ subtitle }}</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
+        <span class="mr-2"></span>
+      </v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <LandingPage
+        v-bind:title="title"
+        v-bind:subtitle="subtitle"
+        v-bind:oneLiner="oneLiner"
+        v-bind:purposes="purposes"
+      />
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+import LandingPage from "./components/LandingPage";
+
+export default {
+  name: "App",
+  components: {
+    LandingPage
+  },
+  data: () => ({
+    title: "behind",
+    subtitle: "what really happens behind the curtain",
+    oneLiner:
+      "이직/구직 희망자들이 익명의 현직자들로부터 생생한 정보를 제공받을 수 있게 연결해주는 서비스",
+    purposes: ["사내 문화 질문", "인터뷰 준비"]
+  })
+};
+</script>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,48 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col cols="12" md="8">
+        <v-row no-gutters justify="center">
+          <v-col cols="6" md="4">
+            <v-img src="@/assets/logo.png" max-width="100%" />
+            <span class="title">{{ title | capitalize }}</span>
+          </v-col>
+        </v-row>
+        <br />
+        {{ oneLiner }}
+        <ul v-for="purpose in purposes" v-bind:key="purpose">
+          <li>{{ purpose }}</li>
+        </ul>
+        <br />
+        <br />
+        <v-text-field label="email" placeholder="behind@behind.co" outlined></v-text-field>
+        <v-btn>서비스 출시시 가장 먼저 연락 받기</v-btn>
+      </v-col>
+      <v-col cols="12" md="4">
+        톡 예제 - 바꿀거
+        <v-img src="@/assets/serviceExample.jpg" max-width="100%" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    oneLiner: String,
+    purposes: Array
+  },
+  filters: {
+    capitalize: function(value) {
+      if (!value) return "";
+      value = value.toString();
+      return value.toUpperCase();
+    }
+  },
+  data: () => ({})
+};
+</script>
+
+<style scoped>
+</style>
