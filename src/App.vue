daa4079f28df2268cd2b4617979d35347b55eab3
Youngjun Choi <yjaquians@gmail.com>
2019-08-28 14:23:45 +0900

    contents, email input updated (no server)

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,9 @@
         v-bind:title="title"
         v-bind:subtitle="subtitle"
         v-bind:oneLiner="oneLiner"
+        v-bind:description="description"
         v-bind:purposes="purposes"
+        v-bind:partnerEmployees="partnerEmployees"
       />
     </v-content>
   </v-app>
@@ -33,9 +35,13 @@ export default {
   data: () => ({
     title: "behind",
     subtitle: "what really happens behind the curtain",
-    oneLiner:
-      "이직/구직 희망자들이 익명의 현직자들로부터 생생한 정보를 제공받을 수 있게 연결해주는 서비스",
-    purposes: ["사내 문화 질문", "인터뷰 준비"]
+    oneLiner: "현직자를 만나는 가장 심플한 방법",
+    description: "현직자와 구직자간의 생생한 Q&A 서비스",
+    purposes: [
+      "진실된 사내 문화에 대한 대화",
+      "회사 맞춤 입사준비/이력서 컨설팅"
+    ],
+    partnerEmployees: ["kakao", "LG전자", "삼성전자", "CJ", "현대카드", "NAVER"] // 로고 이미지로 변환
   })
 };
 </script>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -1,46 +1,88 @@
 <template>
   <v-container>
     <v-row>
-      <v-col cols="12" md="8">
+      <v-col cols="12" md="6">
         <v-row no-gutters justify="center">
-          <v-col cols="6" md="4">
+          <v-col cols="4" md="6">
             <v-img src="@/assets/logo.png" max-width="100%" />
-            <span class="title">{{ title | capitalize }}</span>
           </v-col>
         </v-row>
+        <div>
+          <div class="title text-uppercase">{{ title }}</div>
+          <div>{{ oneLiner }}</div>
+          <div>{{ description }}</div>
+        </div>
         <br />
-        {{ oneLiner }}
-        <ul v-for="purpose in purposes" v-bind:key="purpose">
+        <div
+          v-for="partnerEmployee in partnerEmployees"
+          v-bind:key="partnerEmployee"
+          class="text-center"
+        >{{ partnerEmployee }}에 다니는 사람한테 물어보세요!</div>
+        <br />
+        <!-- <ul v-for="purpose in purposes" v-bind:key="purpose">
           <li>{{ purpose }}</li>
-        </ul>
-        <br />
-        <br />
-        <v-text-field label="email" placeholder="behind@behind.co" outlined></v-text-field>
-        <v-btn>서비스 출시시 가장 먼저 연락 받기</v-btn>
+        </ul>-->
+        <v-row>
+          <v-text-field
+            placeholder="ahead@behind.co"
+            label="email 을 등록해주시면 출시 기념 쿠폰을 보내드립니다"
+            autofocus
+            outlined
+            clearable
+            v-model="userEmail"
+            :rules="emailRules"
+            v-on:keyup.enter="OnRegisterEmail"
+          ></v-text-field>
+          <v-btn outlined block color="indigo" v-on:click="OnRegisterEmail">등록하기</v-btn>
+        </v-row>
       </v-col>
-      <v-col cols="12" md="4">
-        톡 예제 - 바꿀거
-        <v-img src="@/assets/serviceExample.jpg" max-width="100%" />
+      <v-col cols="12" md="6">
+        <v-card>프로토타입사진은요기에놓을겁니다</v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
-
 <script>
 export default {
   props: {
     title: String,
     oneLiner: String,
-    purposes: Array
+    description: String,
+    purposes: Array,
+    partnerEmployees: Array
   },
-  filters: {
-    capitalize: function(value) {
-      if (!value) return "";
-      value = value.toString();
-      return value.toUpperCase();
+  data: function() {
+    return {
+      userEmail: "",
+      emailRules: []
+    };
+  },
+  methods: {
+    OnRegisterEmail: function(e) {
+      e.preventDefault();
+      if (this.validEmail(this.userEmail)) {
+        // store this email (this.userEmail) to DB here
+        // if successful, show following message
+        if (0) {
+          alert(
+            "감사합니다.\n" +
+              this.userEmail +
+              "가 성공적으로 출시 이벤트에 등록되었습니다."
+          );
+        } else {
+          alert("죄송합니다. 무언가 서버가 뻑났습니다. 뻐킹 갓...");
+        }
+      } else {
+        alert(
+          "이벤트 등록에 실패하였습니다.\n입력하신 email 주소를 다시 확인해주세요."
+        );
+      }
+    },
+    validEmail: function(email) {
+      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return re.test(email);
     }
-  },
-  data: () => ({})
+  }
 };
 </script>
 
