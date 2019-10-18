46215267b91baad987d945934fadd9799dcb85db
Youngjun Choi <yjaquians@gmail.com>
2019-10-18 21:26:01 +0900

    add redirects for router to work via netlify

--- src/router.js
+++ src/router.js
@@ -9,6 +9,10 @@ export default new Router({
   base: process.env.BASE_URL,
   routes: [
     {
+      path: '/*',
+      component: Home
+    },
+    {
       path: '/',
       name: 'home',
       component: Home,
